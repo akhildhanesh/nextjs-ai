4971f5c4fb8a0a57ee66d0a6f20557e6e8aa88f6
Akhil Dhanesh <akhildhanesh@icloud.com>
2025-08-06 08:26:25 +0530

    generate text

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,13 +1,26 @@
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
-  const { text } = await generateText({
-    model: google("gemini-2.0-flash"),
-    maxOutputTokens: 50,
-    prompt: "today's date",
-  });
+export async function POST(req: NextRequest) {
+  try {
+    const { prompt } = await req.json();
+    const { text } = await generateText({
+      model: google("gemini-2.0-flash"),
+      maxOutputTokens: 100,
+      prompt: prompt,
+    });
 
-  return NextResponse.json({ text });
+    return NextResponse.json({ text });
+  } catch (error) {
+    console.error("Error:", error);
+    NextResponse.json(
+      {
+        error: "Failed to generate text",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- src/app/ui/completion/page.tsx
+++ src/app/ui/completion/page.tsx
@@ -1,15 +1,73 @@
 "use client"
 
+import { FormEvent, useState } from "react";
+import Markdown from "react-markdown";
+
 const CompletionPage = () => {
+  const [prompt, setPrompt] = useState<string>("")
+  const [completion, setCompletion] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const complete = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await fetch("/api/completion", {
+        method: "POST",
+        body: JSON.stringify({prompt})
+      })
+
+      const data = await response.json();
+      setCompletion(data?.text);
+
+      if (!response.ok) {
+        throw new Error(data?.error)
+      }
+    } catch (error) {
+      console.error(error)
+      setError(error instanceof Error ? error?.message : "Something went wrong")
+    } finally {
+      setIsLoading(false)
+      setPrompt("")
+    }
+  };
   return (
-      <div>
-          
-          <form action="">
-              <input type="text" placeholder="How can i help you?" />
-              <button>Send</button>
-          </form>
+    <div className="flex flex-col items-center">
+      <div className="p-5">
+        {error && !completion ? (
+          <div className="text-red-500">{error}</div>
+        ) : null}
+        {isLoading ? (
+          <div className="text-white animate-pulse font-bold">Loading</div>
+        ) : completion ? (
+          <div className="whitespace-pre-wrap text-white">
+            <Markdown>{completion}</Markdown>
+          </div>
+        ) : null}
+      </div>
+      <form className="fixed bottom-2 w-full px-3 max-w-lg" onSubmit={complete}>
+        <div className="flex gap-2 w-full">
+          <input
+            type="text"
+            placeholder="How can i help you?"
+            className="flex-1 !bg-[#303030] p-2 pl-5 py-3 w-full text-white rounded-full outline-none"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button
+            type="submit"
+            className={`font-bold cursor-pointer ${
+              isLoading || !prompt ? "text-gray-500" : "text-white "
+            }`}
+            disabled={isLoading || !prompt}
+          >
+            Send
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default CompletionPage
